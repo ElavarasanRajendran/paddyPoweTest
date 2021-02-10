a723ed6cfad1a688d4ce5a72edcb6e70f87d32aa
Elavarasan Rajednran <elavarasan821090@gmail.com>
2021-02-10 19:07:25 +0000

    update project details

--- src/main/java/com/paddy/power/test/paddypowertest/PaddyPowerTestApplication.java
+++ src/main/java/com/paddy/power/test/paddypowertest/PaddyPowerTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaddypowerTestApplication {
+public class PaddyPowerTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaddypowerTestApplication.class, args);
+        SpringApplication.run(PaddyPowerTestApplication.class, args);
     }
 
 }
