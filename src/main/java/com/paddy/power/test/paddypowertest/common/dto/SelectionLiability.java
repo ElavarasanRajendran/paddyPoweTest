2245da938b7669a6364e64bb372ba3019c58e12c
Elavarasan Rajednran <elavarasan821090@gmail.com>
2021-02-12 14:37:08 +0000

    feature - add console logger implementation for logging

--- src/main/java/com/paddy/power/test/paddypowertest/common/dto/SelectionLiability.java
+++ src/main/java/com/paddy/power/test/paddypowertest/common/dto/SelectionLiability.java
@@ -2,6 +2,10 @@ package com.paddy.power.test.paddypowertest.common.dto;
 
 import lombok.Data;
 
+import java.text.DecimalFormat;
+
+import static com.paddy.power.test.paddypowertest.common.constants.LoggerConstants.DECIMAL_FORMAT;
+
 @Data
 public class SelectionLiability {
     private String selectionName;
@@ -9,4 +13,18 @@ public class SelectionLiability {
     private int numberOfBets;
     private double totalStakes;
     private double totalLiabilities;
+
+    @Override
+    public String toString() {
+        return new StringBuilder(this.selectionName)
+                .append("\t").append("\t").append(this.currency)
+                .append("\t\t\t")
+                .append("\t").append("\t").append(this.numberOfBets)
+                .append("\t\t\t")
+                .append("\t").append("\t").append(DECIMAL_FORMAT.format(this.totalStakes))
+                .append("\t\t\t")
+                .append("\t").append("\t").append(DECIMAL_FORMAT.format(this.totalLiabilities))
+                .toString();
+    }
+
 }
--- src/main/java/com/paddy/power/test/paddypowertest/common/dto/TotalLiability.java
+++ src/main/java/com/paddy/power/test/paddypowertest/common/dto/TotalLiability.java
@@ -1,11 +1,23 @@
 package com.paddy.power.test.paddypowertest.common.dto;
 
 import lombok.Data;
+import lombok.ToString;
+
+import static com.paddy.power.test.paddypowertest.common.constants.LoggerConstants.DECIMAL_FORMAT;
 
 @Data
+@ToString
 public class TotalLiability {
     private String currency;
     private int numberOfBets;
     private double totalStakes;
     private double totalLiabilities;
+
+    @Override
+    public String toString() {
+        return  new StringBuilder(this.currency)
+                .append("\t\t\t").append(this.numberOfBets)
+                .append("\t\t\t\t\t").append(DECIMAL_FORMAT.format(this.totalStakes))
+                .append("\t\t\t\t\t").append(DECIMAL_FORMAT.format(this.totalLiabilities)).toString();
+    }
 }
